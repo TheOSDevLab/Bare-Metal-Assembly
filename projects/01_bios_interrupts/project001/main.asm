8626dc992df9d753c8bd76441cdade8f4331883b
brogrammer232 <raymondmwaura232@gmail.com>
2025-07-02 20:03:49 +0300

    main.asm: Improved the comments.

--- projects/01_bios_interrupts/project001/main.asm
+++ projects/01_bios_interrupts/project001/main.asm
@@ -2,18 +2,18 @@ org 0x7C00
 bits 16
 
 start:
-    ; Setting video mode to 80x25 text mode (`03h`).
-    mov ah, 0x00    ; Set video mode function.
-    mov al, 0x03    ; 80x25 color text mode.
+    ; Set video mode to 80x25 text mode (`03h`).
+    mov ah, 0x00    ; Function: Set video mode.
+    mov al, 0x03    ; Mode: 80x25 color text.
     int 0x10        ; Call BIOS.
 
-    ; Printing a single character using `BIOS INT 10h`, `0Eh` function.
-    mov ah, 0x0E    ; Teletype function.
+    ; Print a single character using BIOS INT 10h, function 0Eh (TTY output).
+    mov ah, 0x0E    ; Function: Teletype output.
     mov al, 'A'     ; Character to print.
     mov bh, 0       ; Page number.
     int 0x10        ; Call BIOS
 
     hlt             ; Halt the CPU forever.
 
-times 510 - ($ - $$) db 0	; Padding with 0s.
+times 510 - ($ - $$) db 0	; Pad the boot sector with zeros.
 dw 0xAA55			        ; Boot signature.
